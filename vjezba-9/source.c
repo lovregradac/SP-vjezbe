0d118f495b824cd141a84b4838c014d059d368fa
lgrada00 <lgrada00@fesb.hr>
2021-01-11 00:26:31 +0100

    bokte

--- vjezba-9/source.c
+++ vjezba-9/source.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>¸
+#define max(a,b) (((a) > (b)) ? (a) : (b))
 
 struct _element;
 typedef struct _element * Position;
@@ -17,12 +18,12 @@ Element;
 
 Position InitElement(int value);
 Position FindElement(Position Tree, int value);
-Position FindParent(Position Tree, int value);
+Position FindParent(Position Tree, Position Child);
 Position FindMinElement(Position StartingElement);
 
 int DeleteChildFromParent(Position Tree, Position Child);
-int InsertRecursively(Position CurrentElement, Position TargetElement);
-int DeleteElement(Position Tree, Position TargetElement);
+Position InsertRecursively(Position CurrentElement, Position TargetElement);
+Position DeleteElement(Position Tree, Position TargetElement);
 int PreOrder(Position Tree);
 
 void ClearTree(Position Tree);
@@ -56,31 +57,27 @@ int main() {
             if (NewElement == NULL)
                 continue;
 
-            // Ako je stablo prazno, umetni novi element na mjestu prvog (Tree) i preskoči
-            // ostatak petlje. Ovo radimo jer nemamo HEAD.
-            if (NULL == Tree) {
-                Tree = NewElement;
-                printf("\t Element inserted!\n");
-                continue;
-            }
-
-            // Inače, rekurzivno umetni element.
-            if (InsertRecursively(Tree, NewElement) == 0) {
-                printf("\t Element inserted!\n");
-            }
+            // Ubacivanje elementa.
+            Tree = InsertRecursively(Tree, NewElement);
 
             break;
         case '2':
             // Ispisivanje stabla.
             PreOrder(Tree);
+
             printf("\n");
-            break;
+
+            break;       
         case '3':
             // Pronalaženje elementa.
 
             // Unos vrijednosti.
             printf("Enter value: ");
             scanf("%d", & ElementValue);
+
+            if (NULL == Tree)
+                // Nema elemenata za tražiti...
+                continue;
 
             Position TargetElement = FindElement(Tree, ElementValue);
 
@@ -96,13 +93,17 @@ int main() {
 
             if (TargetElement->left != NULL)
                 printf("\t Left child: %d", TargetElement->left->value);
-
             if (TargetElement->right != NULL)
                 printf("\t Right child: %d\n", TargetElement->right->value);
 
             break;
+
         case '4':
             // Brisanje elementa.
+
+            if (NULL == Tree)
+                // Nema elemenata za brisati...
+                continue;
 
             // Unos.
             printf("Unesi element: ");
@@ -110,17 +111,7 @@ int main() {
 
             // Pronalaženje i brisanje.
             Position ElementToDelete = FindElement(Tree, ElementValue);
-            int Delete = DeleteElement(Tree, ElementToDelete);
-
-            if (Delete == 0) {
-                printf("\t Element deleted!\n");
-            } else if (Delete == -1) {
-                // DeleteElement vraća -1 ako je izbrisan korijen koji nema djece. U tom slučaju
-                // Tree moramo postavit na NULL jer je stablo sada prazno a nemamo HEAD.
-                Tree = NULL;
-                printf("\t Element deleted!\n");
-            } else
-                printf("\t Deletion failed!\n");
+            Tree = DeleteElement(Tree, ElementToDelete);
 
             break;
         }
@@ -148,85 +139,57 @@ void ClearTree(Position Tree){
     free(Tree);
 }
 
-int DeleteElement(Position Tree, Position TargetElement) {
-    // Funkcija za brisanje elementa.
+Position DeleteElement(Position Tree, Position TargetElement) {
+    // Rekurzivna unkcija za brisanje elementa.
 
     // Imamo 3 moguća slučaja:
-    //      1. Element nema djece.
-    //      2. Element ima 2 dijeteta.
-    //      3. Element ima 1 dijete.
+    //      1. Element ima 2 dijeteta.
+    //      2. Element ima 1 dijete.
+    //      3. Element nema djece.
 
-    // 1. Ako element nema djece, postavimo pokazivač njegovog roditelja na NULL i oslobodimo
-    //    alociranu memoriju.
-    if (TargetElement->left == NULL && TargetElement->right == NULL) {
-        if (Tree == TargetElement) {
-             // Slučaj ako želimo izbrisati korijen (ne tražimo roditelja, već samo brišemo element).
-             free(Tree);
-             // Šaljem -1 nazad jer sam zakompliciro (main treba postavit Tree na NULL)...
-             return -1;
-        } 
-        else
-            // Slučaj ako želimo izbrisati element koji nije korijen.
-            DeleteChildFromParent(Tree, TargetElement);
-            return 0;
-    }
-    // 2. Ako element ima 2 dijeteta, zamijenimo ga sa najmanjim elementom u njegovom desnom
-    //    podstablu te izbrišemo taj element.
-    else if (TargetElement->left != NULL && TargetElement->right != NULL) {
-        // Najmanji element u desnom podstablu elementa kojeg brišemo.
-        Position MinElement = FindMinElement(TargetElement->right);
+    if (NULL == Tree)
+        return Tree;
 
-        // Ne smijemo odmah dodijeliti vrijednost minimalnog elementa iz desnog podstabla onom
-        // elementu kojeg brišemo, jer ćemo tada imati 2 elementa s istom vrijednosti, pa ga
-        // spremamo u temp varijablu.
-        int TempValue = MinElement->value;
-
-        // Izbriši minimalni element iz desnog podstabla.
-        DeleteElement(Tree, MinElement);
-        // Dodijeli njegovu vrijednost elementu kojeg brišemo.
-        TargetElement->value = TempValue;
-        return 0;
-    }
-    // 3. Ako element ima 1 dijete, taj element zamijenimo s njegovim dijetetom.
+    // Ako je vrijednost čvora veća od vrijednosti elementa kojeg brišemo, rekurzija ide lijevo.
+    if (Tree->value > TargetElement->value)
+        Tree->left = DeleteElement(Tree->left, TargetElement);
+    // Ako je vrijednost čvora manja od vrijednosti elementa kojeg brišemo, rekurzija ide desno.
+    else if (Tree->value < TargetElement->value)
+        Tree->right = DeleteElement(Tree->right, TargetElement);
+    // Inače, vrijednosti su jednake pa smo pronašli element kojeg brišemo.
     else {
-        Position Child = NULL;
+        // 1. Slučaj kada element ima dvoje djece.
+        if (Tree->left != NULL && Tree->right != NULL) {
+            // Pronalazimo najmanji element u desnom podstablu elementa.
+            Position MinElement = FindMinElement(Tree->right);
 
-        // Znamo da element kojeg brišemo ima 1 dijete. Gledamo s koje strane se nalazi te spremamo
-        // njegovu adresu u Child.
-        if (TargetElement->left != NULL)
-            Child = TargetElement->left;
-        else if (TargetElement->right != NULL)
-            Child = TargetElement->right;
+            // Mijenjamo vrijednost trenutnog elementa.
+            Tree->value = MinElement->value;
 
-        // Ako je element korijen stabla, ne trebamo tražiti roditelja, samo ga zamijenimo sa
-        // dijetetom.
-        if (Tree == TargetElement) {
-            Tree->value = Child->value;
-            Tree->left = Child->left;
-            Tree->right = Child->right;
-            free(Child);
-            return 0;
+            // Brišemo najmanji element iz desnog podstabla.
+            Tree->right = DeleteElement(Tree->right, MinElement);
         }
-        // Ako element nije korijen stabla, tražimo njegovog roditelja, uspostavljamo vezu s
-        // dijetetom pa brišemo element.
         else {
-            // Pronalazimo roditelja od elementa kojeg želimo izbrisati.
-            Position Parent = FindParent(Tree, TargetElement->value);
+            Position Temp = Tree;
 
-            // Provjeravamo s koje strane roditelja se nalazi element kojeg brišemo, te umjesto
-            // njega insertiramo njegovo dijete (Child).
-            if (Parent->left == TargetElement)
-                Parent->left = Child;
-            else if (Parent->right == TargetElement)
-                Parent->right = Child;
-            
-            // Brišemo element.
-            free(TargetElement);
-            return 0;
+            // Uvjet za upravljanje sa preostalim slučajevima.
+            // 2. Element ima 1 dijete:
+            //      Ako je lijevo dijete NULL, znači da desno nije, pa element kojeg brišemo poprima
+            //      njegovu vrijednost. Element zatim brišemo.
+            // 3. Element nema djece: 
+            //      Brišemo element, uvjet nema utjecaja.
+            if (Tree->left == NULL)
+                Tree = Tree->right;
+            else
+                Tree = Tree->left;
+
+            free(Temp);
         }
     }
 
-    return 1;
+    // Funkcija će uvijek vratiti vrijednost s kojom je prvotno pozvana kako bi korijen ostao
+    // sačuvan.
+    return Tree;
 }
 
 Position FindMinElement(Position StartingElement) {
@@ -239,70 +202,18 @@ Position FindMinElement(Position StartingElement) {
     FindMinElement(StartingElement->left);    
 }
 
-int DeleteChildFromParent(Position Tree, Position Child) {
-    // Funkcija za brisanje dijeteta tj. veze između roditelja i dijeteta.
-
-    // Prvo pronalazimo roditelja čije dijete ime specificiranu vrijednost (value).
-    Position Parent = FindParent(Tree, Child->value);
-
-    // Provjeravamo s koje strane roditelja se nalazi dijete, pa ga onda brišemo.
-    if (Parent->left == Child) {
-        Child = Parent->left;
-        Parent->left = NULL;
-        free(Child);
-        return 0; 
-    }
-    else if (Parent->right == Child) {
-        Child = Parent->right;
-        Parent->right = NULL;
-        free(Child);
-        return 0;
-    }
-
-    return 1;
-}
-
-Position FindParent(Position Tree, int value) {
-    // Rekurzivna unkcija za pronalaženje roditelja dijeteta s specificiranom vrijednosti (value).
-    // Tree je element na kojem se nalazimo u trenutnoj razini rekurzije.
-
-    // Ako Tree ima lijevo dijete, provjeravamo je li njegova vrijednost jednaka traženoj
-    // vrijednosti. Ako je, Tree je roditelj pa izlazimo iz rekurzije.
-    if (Tree->left != NULL)
-        if (Tree->left->value == value)
-            return Tree;
-    
-    // Isto za desnu tranu.
-    if (Tree->right != NULL)
-        if (Tree->right->value == value)
-            return Tree;
-
-    // Ukoliko trenutni Tree nije roditelj kojeg tražimo, funkcija se širi na lijevu ili desnu
-    // stranu, ovisno o vrijednosti.
-    if (Tree != NULL) {
-        if (Tree->value > value)
-            FindParent(Tree->left, value);
-        else if (Tree->value < value)
-            FindParent(Tree->right, value);
-    }
-    
-}
-
 Position FindElement(Position Tree, int value) {
     // Funkcija za rekurzivnu pretragu binarnog stabla.
 
     // Vrati trenutni element u rekurziji ako je njegova vrijednost jednaka traženoj.
-    if (Tree->value == value) {
+    if (Tree->value == value)
         return Tree;
-    }
     // Nastavi rekurziju lijevo ako je vrijednost manja od vrijednosti trenutnog elementa.
-    else if (Tree->value > value) {
+    else if (Tree->value > value)
         FindElement(Tree->left, value);
-    }
     // Nastavi rekurziju desno ako je vrijednost manja od vrijednosti trenutnog elementa.
-    else {
+    else
         FindElement(Tree->right, value);
-    }
 }
 
 int PreOrder(Position Tree) {
@@ -317,38 +228,26 @@ int PreOrder(Position Tree) {
         return 0;
 }
 
-int InsertRecursively(Position CurrentElement, Position TargetElement) {
-    // Rekurzivna funkcija za umetanje elementa u stablo.
+Position InsertRecursively(Position CurrentElement, Position TargetElement) {
+    // Rekurzivna funkcija za umetanje novog elementa u stablo.
 
-    // Umetanje u desno podstablo ako je novi element veći od čvora.
-    if (TargetElement->value > CurrentElement->value) {
-        // Ako je desni element NULL, umetni tu.
-        if (CurrentElement->right == NULL) {
-            CurrentElement->right = TargetElement;
-            return 0;
-        }
-        // Inače nastavi tražiti mjesto.
-        InsertRecursively(CurrentElement->right, TargetElement);
-    }
-
-    // Umetanje u lijevo podstablo ako je novi element manji od čvora.
-    if (TargetElement->value < CurrentElement->value) {
-        // Ako je lijevi element NULL, umetni tu.
-        if (CurrentElement->left == NULL) {
-            CurrentElement->left = TargetElement;
-            return 0;
-        }
-        // Inače nastavi tražiti mjesto.
-        InsertRecursively(CurrentElement->left, TargetElement);
-    }
-
-    // Ako naiđemo na element s istom vrijednosti...
-    if (TargetElement->value == CurrentElement->value) {
+    if (CurrentElement == NULL)
+        // Ako smo naišli na NULL, vraćamo element kojeg insertiramo i time zaustavljamo rekurziju,
+        // što znači da će određeni čvor poprimiti adresu TargetElement.
+        return TargetElement;
+    else if (CurrentElement->value < TargetElement->value)
+        // Ako je vrijednost trenutnog čvora manja od vrijednosti elementa kojeg ubacivamo,
+        // rekurzija ide desno.
+        CurrentElement->right = InsertRecursively(CurrentElement->right, TargetElement);
+    else if (CurrentElement->value > TargetElement->value)
+        // Ako je vrijednost trenutnog čvora veća od vrijednosti elementa kojeg ubacivamo,
+        // rekurzija ide lijevo.
+        CurrentElement->left = InsertRecursively(CurrentElement->left, TargetElement);
+    else 
+        // Ako naiđemo na čvor s istom vrijednosti...
         printf("\t Element already exists!\n");
-        return 1;
-    }
 
-    return 0;
+    return CurrentElement;
 }
 
 void PrintMenu() {
